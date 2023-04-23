192bab7a16eb7da399fb59e892b04b4e81ccbb0a
Dustyn Eggers <Eggers.cgi@swr.de>
2023-04-23 21:38:06 +0200

    3.3.3 Implementing the health_check handler and route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web :: {get, App, HttpResponse, HttpServer, Responder};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(health_check)
+    })
+        .bind(("127.0.0.1", 8080))
+        .expect("Port taken")
+        .run()
+        .await
+}
+
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
